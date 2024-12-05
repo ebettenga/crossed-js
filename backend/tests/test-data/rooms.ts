039ba1b4ee2ab84e5a243f5b91303469aeb485dc
Ethan Bettenga <ethan@kindlyhuman.com>
2024-12-05 07:29:19 -0600

    maybe works

--- backend/tests/test-data/rooms.ts
+++ backend/tests/test-data/rooms.ts
@@ -3,26 +3,42 @@ import { Room } from '../../src/entities/Room';
 import { User } from '../../src/entities/User';
 import { Crossword } from '../../src/entities/Crossword';
 
+
+const testUsers = [
+  { username: 'testplayer1', githubId: "john123", role: 'USER' },
+  { username: 'testplayer2', githubId: "jane456", role: 'USER' },
+]
+
+
+const testCrosswords = [
+  {
+    clues: { across: [], down: [] },
+    answers: { across: [], down: [] },
+    author: 'Test Author',
+    circles: [],
+    date: new Date(),
+    dow: 'Monday',
+    grid: ["x", "x", "x", "x", "x"],
+    gridnums: [],
+    shadecircles: false,
+    col_size: 4,
+    row_size: 4,
+    jnote: 'The quick brown fox jumps over the lazy dog',
+    notepad: 'Test notepad',
+    title: 'Test Crossword',
+  },
+]
+
 export const create = async (connection: DataSource) => {
   const roomRepository = connection.getRepository(Room);
   const userRepository = connection.getRepository(User);
   const crosswordRepository = connection.getRepository(Crossword);
 
-  const user1 = await userRepository.findOne({ where: { username: 'testuser' } });
-  const user2 = await userRepository.findOne({ where: { username: 'testadmin' } });
-  const crossword = await crosswordRepository.findOne({ where: { title: 'Test Crossword' } });
+  const user1 = await userRepository.save(testUsers[0]);
+  const user2 = await userRepository.save(testUsers[1]);
+  const crossword = await crosswordRepository.save(testCrosswords[0]);
 
-  if (!user1 || !user2 || !crossword) {
-    console.log({
-      user1,
-      user2,
-      crossword,
-    })
-    console.error('Test data for rooms init script not found.');
-    return;
-  }
-
-  const room = roomRepository.create({
+  await roomRepository.save({
     player_1: user1,
     player_2: user2,
     crossword,
@@ -31,7 +47,5 @@ export const create = async (connection: DataSource) => {
     player_1_score: 0,
     player_2_score: 0,
   });
-
-  await roomRepository.save(room);
   console.log('Test room created.');
 };
