21cde87599d5c737a136686f1ffae8f7a4bb2217
Ethan Bettenga <ebettenga@gmail.com>
2025-01-05 18:20:31 -0600

    theme update

--- frontend/config/theme.ts
+++ frontend/config/theme.ts
@@ -22,7 +22,7 @@ export const theme = {
     border: {
       DEFAULT: '#E5E5E5',
       light: '#EBEBEB',
-      dark: 'rgb(75 85 99)',
+      dark: '#E6E6E6',
     },
     status: {
       success: '#34D399',
