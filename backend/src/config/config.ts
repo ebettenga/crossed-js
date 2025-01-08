ccfff7b4ab9da73539198f3a3c989e91c4f750a9
Ethan Bettenga <ebettenga@gmail.com>
2025-01-08 17:12:19 -0600

    changes needed

--- backend/src/config/config.ts
+++ backend/src/config/config.ts
@@ -1,19 +1,19 @@
-import { commonConfig } from './config-common';
-import { config as localConfig } from './config-local';
-import { config as ciConfig } from './config-ci';
-import { config as productionConfig } from './config-production';
+import { commonConfig } from "./config-common";
+import { config as localConfig } from "./config-local";
+import { config as ciConfig } from "./config-ci";
+import { config as productionConfig } from "./config-production";
 export const getConfig = () => {
   const env = process.env.NODE_ENV;
 
   switch (env) {
-    case 'production':
+    case "production":
       return { ...commonConfig, ...productionConfig };
-    case 'ci':
-      return { ...commonConfig, ...ciConfig };
     default:
+      if (process.env.CI === "true") {
+        return { ...commonConfig, ...ciConfig };
+      }
       return { ...commonConfig, ...localConfig };
   }
-
 };
 
 export const config = getConfig();
