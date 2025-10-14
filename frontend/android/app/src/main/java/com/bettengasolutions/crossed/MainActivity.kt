b9c2d068549f800ff4961e49d79e94d3a1d21ff9
Ethan Bettenga <ebettenga@gmail.com>
2025-10-13 22:54:31 -0500

    building for production

--- frontend/android/app/src/main/java/com/bettengasolutions/crossed/MainActivity.kt
+++ frontend/android/app/src/main/java/com/bettengasolutions/crossed/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.anonymous.crossed
+package com.bettengasolutions.crossed
 import expo.modules.splashscreen.SplashScreenManager
 
 import android.os.Build
--- frontend/android/app/src/main/java/com/bettengasolutions/crossed/MainApplication.kt
+++ frontend/android/app/src/main/java/com/bettengasolutions/crossed/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.anonymous.crossed
+package com.bettengasolutions.crossed
 
 import android.app.Application
 import android.content.res.Configuration
