674192e1f40f91e67357eaebeba2e64ce73efded
Ethan Bettenga <ebettenga@gmail.com>
2025-10-13 23:05:11 -0500

    safe area view updates for styles

--- frontend/app/(auth)/_layout.tsx
+++ frontend/app/(auth)/_layout.tsx
@@ -14,7 +14,7 @@ export default function AuthLayout() {
   }
 
   return (
-    <SafeAreaView className="bg-white h-full">
+    <SafeAreaView className="flex-1 bg-[#F6FAFE] dark:bg-[#0F1417] h-full">
       <Slot />
     </SafeAreaView>
   );
--- frontend/app/(root)/_layout.tsx
+++ frontend/app/(root)/_layout.tsx
@@ -4,7 +4,7 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function RootLayout() {
   return (
-    <SafeAreaView className="bg-white h-full">
+    <SafeAreaView className="flex-1 bg-[#F6FAFE] dark:bg-[#0F1417] h-full">
       <Slot />
     </SafeAreaView>
   );
